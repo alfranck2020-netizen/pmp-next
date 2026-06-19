a65168fa8c2934a63e051014f26e3a4e06cd0286
alfranck2020-netizen <alfranck2020@gmail.com>
2026-06-19 15:56:15 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,42 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+
+const GA_ID = 'G-442N7HEQV8';
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (typeof window.gtag !== 'undefined') {
+        window.gtag('config', GA_ID, { page_path: url });
+      }
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
+  }, [router.events]);
+
+  return (
+    <>
+      {/* Google Analytics GA4 */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}', { page_path: window.location.pathname });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
